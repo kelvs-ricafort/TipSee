9c1a556b4861dac25c4640d8a7819395a5299a8b
Kelvin C. Ricafort <kelvs.r@icloud.com>
2023-07-22 14:45:34 +0800

    Create IBActions and IBOutlets for the Calculator Screen

---
+++ TipSee/Controllers/CalculatorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TipSee
+//
+//  Created by Kelvin Ricafort on 7/21/23.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+}
+
